4017fb5d2971713dffb8b52eced2ffd2ab3baf83
Qixiang Fang <fqixiang@gmail.com>
2026-06-09 13:15:10 +0200

    Update dataset groupings in paths.R
    Tidy and update dataset groupings: remove BR_PT_999625 from br_pt; trim first_stage to remove EN_277273, ES_277273 and a duplicate CH_277273 and add a comment noting IT_auto uses second-stage scales; remove obsolete first_stage_ch/first_stage_df entries and rename to_remove to datasets_to_remove for clarity.

--- config/paths.R
+++ config/paths.R
@@ -13,18 +13,17 @@ DIR_MERGED <- file.path(PROJECT_ROOT, "merged")
 
 # Dataset groupings
 DATASETS <- list(
-  br_pt = c("br_pilot", "BR_PT_277273", "BR_PT_999625"),
+  br_pt = c("br_pilot", "BR_PT_277273"),
   ch = c("CH_277273"),
   us = c("US_216254", "US_868141"),
   ch_us_10_min = c("CH_277273", "US_868141"),
   pilot = c("br_pilot"),
+  # first-stage datasets (all except IT_auto, which uses second-stage LPS and FTOS short scales)
   first_stage = c(
-    "CH_277273", "EN_277273", "ES_277273", "IT_277273",
-    "BR_PT_277273", "SL_277273",  "US_216254", "US_868141", "CH_277273"
+    "CH_277273", "IT_277273",
+    "BR_PT_277273", "SL_277273",  "US_216254", "US_868141"
   ),
   first_stage_br_pt = c("BR_PT_277273"),
-  first_stage_ch = c("CH_277273"),
-  first_stage_df = c("EN_277273", "ES_277273", "IT_277273", "BR_PT_277273", "SL_277273"),
-  # following list of datasets to remove
-  to_remove = c("NL_999625", "BR_PT_999625", "CH_999625", "EN_277273", "EN_999625", "ES_277273", "IT_999625", "MS_999625")
+  # list of datasets to remove
+  datasets_to_remove = c("NL_999625", "BR_PT_999625", "CH_999625", "EN_277273", "EN_999625", "ES_277273", "IT_999625", "MS_999625")
 )
